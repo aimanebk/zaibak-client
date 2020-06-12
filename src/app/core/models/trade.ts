0aa85445fcca3c5951cd998d6f1c03379fea8760
ELBAKKALI AIMANE <aimane.elb@gmail.com>
2020-06-13 00:27:23 +0100

    trades log feature implemented

---
+++ src/app/core/models/trade.ts
@@ -0,0 +1,10 @@
+export class Trade {
+    _id: string;
+    article: string;
+    code: string;
+    date: Date;
+    description: string;
+    price: number;
+    quantity: number;
+    username: string;
+}
