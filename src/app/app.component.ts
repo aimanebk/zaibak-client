1894e8a1bbd7a8b108ad532a79bcee87fa641415
ELBAKKALI AIMANE <aimane.elb@gmail.com>
2020-06-29 23:56:54 +0100

    Fix navbar with lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { User } from './core/models/user';
 import { AuthenticationService } from './core/services/authentication.service';
 import { Role } from './core/models/role';
 import { take } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from '@angular/router';
 
 
 @Component({
@@ -13,10 +13,22 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   user: User;
+  showNavbar : boolean = false;
 
   constructor(private authenticationService: AuthenticationService, private router : Router) {
       this.authenticationService.currentUser.subscribe(x => this.user = x);
       this.authenticationService.initRoles().pipe(take(1)).subscribe(x => {});
+      router.events.subscribe(e => {
+        if (e instanceof RouteConfigLoadStart) {
+            // show Navbar
+            this.showNavbar = false;
+        } else if (e instanceof RouteConfigLoadEnd ) {
+            // hide navbar
+            this.showNavbar = true;
+
+        }
+    });
+    
   }
 
   get isAdmin() {
